7601d1bf8dfcc5395cd90642d354dfbcf84d5ead
Evan Schafer <evanschafer@Evans-MacBook-Pro.local>
2026-05-07 14:49:42 -0700

    Set up game screen skeleton

---
+++ app/InGame/MissionPlayScreen/MissionPlayScreen.tsx
@@ -0,0 +1,13 @@
+import "./MissionPlayScreen.scss"
+
+export const MissionPlayScreen = () => {
+    return (
+        <div className="mission-play-screen-container">
+            <div className="top">top</div>
+            <div className="left">left</div>
+            <div className="center">center</div>
+            <div className="right">right</div>
+            <div className="bottom">bottom</div>
+        </div>
+    )
+}
